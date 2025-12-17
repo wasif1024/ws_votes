d5e2722b7b2f50dabb2f1b5a4a5a87ff6980501a
wasif1024 <wasif1024@gmail.com>
2025-12-18 01:06:02 +0400

    wallets and connections set

--- programs/ws_votes/src/lib.rs
+++ programs/ws_votes/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{accounts::interface_account, prelude::*};
 
 declare_id!("13cPJfvm6R4Z965utk47dDgemVdGVN2wSbgE9zZ1zHzg");
 
@@ -6,11 +6,33 @@ declare_id!("13cPJfvm6R4Z965utk47dDgemVdGVN2wSbgE9zZ1zHzg");
 pub mod ws_votes {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_voting(ctx: Context<InitializePoll>,poll_id:u64,poll_start:u64,poll_end:u64,description:String) -> Result<()> {
+        //msg!("Greetings from: {:?}", ctx.program_id);
+        let poll=&mut ctx.accounts.poll;
+        poll.poll_id=poll_id;
+        poll.poll_start=poll_start;
+        poll.poll_end=poll_end;
+        poll.description=description;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info> {
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    #[account(init,payer=signer,space=8+Poll::INIT_SPACE,seeds=[poll_id.to_le_bytes().as_ref()],bump)]
+    pub poll:Account<'info,Poll>,
+    pub system_program:Program<'info,System>
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Poll{
+pub poll_id:u64,
+#[max_len(32)]
+pub description:String,
+pub poll_start:u64,
+pub poll_end:u64,
+pub candidate_amount:u64
+}
--- tests/ws_votes.ts
+++ tests/ws_votes.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { WsVotes } from "../target/types/ws_votes";
+import {PublicKey} from "@solana/web3.js";
+import type { WsVotes } from "../target/types/ws_votes";
+import { BN } from "@coral-xyz/anchor";
+import idl from "../target/idl/ws_votes.json";
 
+//const idl=require("../target/deploy/ws_vote.json");
+//const voting_address=new PublicKey("13cPJfvm6R4Z965utk47dDgemVdGVN2wSbgE9zZ1zHzg");
 describe("ws_votes", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const { connection } = provider;
+  const wallet = provider.wallet;
+  
+  const providerInstance = new anchor.AnchorProvider(connection, wallet, {});
+  anchor.setProvider(providerInstance);
+  //const program = anchor.workspace.wsVotes as Program<WsVotes>;
+  const program = new Program(idl as WsVotes, providerInstance);
 
-  const program = anchor.workspace.wsVotes as Program<WsVotes>;
-
-  it("Is initialized!", async () => {
+  it("Poll Initialization", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log("Program Id now",program.programId.toBase58());
+    const pollId = new BN(1);
+    const pollStart = new BN(2);
+    const pollEnd = new BN(3);
+    const description = "hhg";
+    
+    /*const tx = await program.methods
+      .initializeVoting(pollId, pollStart, pollEnd, description)
+      .rpc();
+    console.log("Your transaction signature", tx);*/
   });
 });
